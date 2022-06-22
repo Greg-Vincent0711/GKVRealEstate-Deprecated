faa5a7ef480caebcc22c6b00aede4e567dbbe49f
Gregory Vincent <52324737+GregV-crypto@users.noreply.github.com>
2022-06-21 21:06:27 -0400

    Add files via upload

---
+++ src/homePage/homePage.jsx
@@ -0,0 +1,26 @@
+/**
+ * @author Gregory Vincent
+ * home-page component
+ */
+import "./homePage.css";
+import HomePageBtn from "./Home-PageBtn/Home-PageBtn";
+import ImageGallery from "./ImageGallery/imageGallery";
+/**
+ * TODO - get images to show up, style them
+ * TODO - change the website's color palette to match the images
+ * TODO - Slider for all the images on the homepage
+ */
+function HomePage() {
+  return (
+    <div className="homepage-container">
+      <h2 className="tagLine fade-inTag">Welcome to our community.</h2>
+      <ImageGallery />
+      <HomePageBtn />
+    </div>
+  );
+}
+
+/**
+ * <img src={TestImage} alt="test" className="fade-in" />
+ */
+export default HomePage;
