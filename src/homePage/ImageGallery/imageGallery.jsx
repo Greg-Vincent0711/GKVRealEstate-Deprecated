4481a3780aa99eab7e9f94694e40f43d086f8dd1
Greg Vincent <gavincent@albany.edu>
2022-06-30 11:02:30 -0400

    Updating Image Gallery

--- src/homePage/ImageGallery/imageGallery.jsx
+++ src/homePage/ImageGallery/imageGallery.jsx
@@ -7,70 +7,134 @@
 import ImageZero from "./Images/TestImageOne.png";
 import ImageOne from "./Images/TestImageTwo.png";
 import ImageTwo from "./Images/TestImageThree.png";
+import PropImageOne from "./Images/PropImage#1.png";
+import PropImageTwo from "./Images/PropImage#2.png";
+import PropImageThree from "./Images/PropImage#3.png";
 import React, { useState } from "react";
 import "./imageGallery.css";
+// import { render } from "@testing-library/react";
 
 /**
  * TODO - automatic image scrolling
  */
 const ImageGallery = () => {
-  const [index, setIndex] = useState(0);
-  const pictureList = [ImageZero, ImageOne, ImageTwo];
-
-  function updateIndex(mathSymbol) {
-    switch (mathSymbol) {
-      case "decrement":
-        setIndex(index - 1);
-        break;
-      case "increment":
-        setIndex(index + 1);
-        break;
-      case "toZero":
-        setIndex(0);
-        break;
-      case "EOA":
-        setIndex(pictureList.length - 1);
-        break;
-      default:
-        return 0;
-    }
-  }
-
-  /**logic for handling the next image*/
-  const clickNext = () => {
-    //check list position before incrementing
-    if (index + 1 === pictureList.length) {
-      setIndex(updateIndex("toZero"));
-    } else {
-      updateIndex("increment");
-    }
-  };
-
-  /**logic for handling the previous image*/
-  const clickPrev = () => {
-    if (index - 1 === -1) {
-      setIndex(updateIndex("EOA"));
-    } else {
-      setIndex(updateIndex("decrement"));
-    }
-  };
+  const [currentImage, setCurrentImage] = useState(0);
+  const pictureList = [
+    //each image can be represented as a js object
+    {
+      //Each heading will correspond to a building address
+      //right now they're placeholders
+      image: ImageZero,
+      heading: "Zero Image",
+      Address: "2521 Watt Street",
+    },
+    {
+      image: ImageOne,
+      heading: "First Image",
+      Address: "2522 Watt Street",
+    },
+    {
+      image: ImageTwo,
+      heading: "Second Image",
+      Address: "2523 Watt Street",
+    },
+    {
+      image: PropImageOne,
+      heading: "Third Image",
+      Address: "2524 Watt Street",
+    },
+    {
+      image: PropImageTwo,
+      heading: "Fourth Image",
+      Address: "2525 Watt Street",
+    },
+    {
+      image: PropImageThree,
+      heading: "Fifth Image",
+      Address: "2520 Watt Street",
+    },
+  ];
 
   return (
     <div className="img-main-container">
-      <img src={pictureList[index]} alt="test" />
       <h1 className="tagLine fade-inTag">Welcome to our community.</h1>
       <h3 className=" h3 fade-in">
         Tenants, click the link below to see your information.
       </h3>
-      <div className="btnGroup"></div>
+      {pictureList.map((picture, index) => {
+        return (
+          <div
+            className={index === currentImage ? "picture current" : "picture"}
+            key={index}
+          >
+            {index === currentImage && (
+              <div>
+                <img src={picture.image} alt="picture" />
+                <div className="picture-info">
+                  <h2>{picture.Address}</h2>
+                </div>
+              </div>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
-
-/*functions refer to all fade in animations*/
-
 export default ImageGallery;
 
+/*functions refer to all fade in animations*/
+// function updateImage(mathSymbol) {
+//   switch (mathSymbol) {
+//     case "-":
+//       setCurrentImage((currentImage -= 1));
+//       break;
+//     case "+":
+//       setCurrentImage((currentImage += 1));
+//       console.log("currentImage is a plus sign");
+//       break;
+//     case "0":
+//       setCurrentImage(0);
+//       break;
+//     case "E":
+//       setCurrentImage(pictureList.length - 1);
+//       break;
+//     default:
+//       return 0;
+//   }
+// }
+
+/**logic for handling the next image*/
+// const NextImg = () => {
+//   //check list position before incrementing
+//   if (currentImage + 1 === pictureList.length) {
+//     setCurrentImage(updateImage("toZero"));
+//   } else {
+//     updateImage("increment");
+//   }
+// };
+
+// /**logic for handling the previous image*/
+// const PrevImg = () => {
+//   if (currentImage - 1 === -1) {
+//     setCurrentImage(updateImage("EOA"));
+//   } else {
+//     setCurrentImage(updateImage("decrement"));
+//   }
+// };
+
+// function imageSwitch() {
+//   if (currentImage === pictureList.length - 1) {
+//     updateImage("0");
+//     console.log(currentImage);
+//   }
+//   updateImage("+");
+//   console.log(currentImage);
+// }
+// function autoGallery() {
+//   setInterval((imageSwitch, 5000));
+//   return currentImage;
+// }
 // <button className="next " onClick={clickNext}></button>;
 /**
  *<button classname="prev " onClick={clickNext}>
