c20e69112a67b6f5257d596dbe543949bb9b9978
Greg Vincent <gregvjrr@gmail.com>
2022-07-28 10:28:41 -0400

    Testing work on a branch.

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-/**
- * html rendering engine
- */
-app.set("view engine", "ejs");
+// /**
+//  * html rendering engine
+//  */
+// app.set("view engine", "ejs");
 
-/**
- * '/' is the root of the directory
- */
-app.get("/GregV-crypto/RealEstateRep", (req, res) => {
-  console.log("Here");
-  //send method seems pretty important
-  // res.send("Hello");
-  res.render("index", { text: "This text is coming from the server" });
-});
+// /**
+//  * '/' is the root of the directory
+//  */
+// app.get("/GregV-crypto/RealEstateRep", (req, res) => {
+//   console.log("Here");
+//   //send method seems pretty important
+//   // res.send("Hello");
+//   res.render("index", { text: "This text is coming from the server" });
+// });
 
-//incorporate the other created routes
-const userRouter = require("./routes/user");
+// //incorporate the other created routes
+// const userRouter = require("./routes/user");
 
-//all outside created routes start with "users" as their route
-app.use("/users", userRouter);
+// //all outside created routes start with "users" as their route
+// app.use("/users", userRouter);
 
-//listen for info on this port
-app.listen(3000);
+// //listen for info on this port
+// app.listen(3000);
