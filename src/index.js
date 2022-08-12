069b18ad112ce95e1a3cda765bd11b48ee9ca27e
Greg Vincent <gavincent@albany.edu>
2022-08-12 10:52:31 -0400

    Commiting changes for Vercel Deployment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+/**Splash Page is a SPA */
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
